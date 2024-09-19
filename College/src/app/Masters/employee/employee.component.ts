d997261e23dbbaf5ce2a0bf38723b42047a3810a
naresh <naresh.swargam@gvrinfosystems.com>
2024-09-19 18:01:09 +0530

    chaitanya employee details

--- College/src/app/Masters/employee/employee.component.ts
+++ College/src/app/Masters/employee/employee.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from '../../sidenav/sidenav.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [SidenavComponent,NgFor,NgIf],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+
+  Countries: string[] = ['India', 'South Africa']; // Fixed array declaration
+  selectedOption: string | undefined;
+
+  onSelect(event: Event) {
+    const target = event.target as HTMLSelectElement; // Type the event target
+    this.selectedOption = target.value; // Now target.value is recognized
+    console.log('Selected:', this.selectedOption);
+  }
+
+  States: string[] = ['India', 'South Africa']; // Fixed array declaration
+  selectedState: string | undefined;
+
+  onStateSelect(event: Event) {
+    const target = event.target as HTMLSelectElement; // Type the event target
+    this.selectedOption = target.value; // Now target.value is recognized
+    console.log('Selected:', this.selectedOption);
+  }
+  
+
 
 }
